0508dbe4e4d1868db3e27c976e8082eca1fddbb7
TanGlennda <t.glennda@gmail.com>
2016-08-17 15:25:23 +0800

    no message

--- Base/Source/Enemy_Psychic.h
+++ Base/Source/Enemy_Psychic.h
@@ -13,12 +13,6 @@ public:
 
 	static Enemy_Psychic* pointer() { return c_enemyPsychic; };
 
-	//enum GEOMETRY_TYPE
-	//{
-	//	//GEO_TILEENEMY_FRAME0,
-	//	NUM_GEOMETRY,
-	//};
-
 	void Init();
 	void Update(double dt, Map map);
 	void RenderPsychic();
@@ -36,6 +30,10 @@ private:
 	bool playerIntrude;
 	bool defMechanism;
 	bool lastResort;
+
+	Mesh* camou_psychic;
+	Mesh* run_psychic;
+	Mesh* appear_psychic;
 };
 
 #endif
