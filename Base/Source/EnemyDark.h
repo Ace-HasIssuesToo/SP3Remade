2ed4b75da42c3ab71ef4d8bb8379043030c89343
Tristan Chia Shi-Jian <150513M@mymail.nyp.edu.sg>
2016-08-18 10:15:57 +0800

    Sprite animations. Done

--- Base/Source/EnemyDark.h
+++ Base/Source/EnemyDark.h
@@ -10,12 +10,26 @@ class Enemy_Dark
 {
 public:
 	virtual void Init();
-	virtual void Update(double dt, Map map);
+	virtual void Update(double dt, Map* map);
+
 	virtual void RenderEnemyDark();
 	virtual void RenderDarkball();
+
 	Vector3 getEnemyDarkPos();
 	Vector3 getEnemyDarkPosOffSet();
 	Vector3 getEnemyDarkScale();
+
+	enum EnemyDarkMeshes
+	{
+		Left = 0,
+		Right,
+		Top,
+		Down
+	};
+
+	void setEnemyDarkMesh(Enemy_Dark::EnemyDarkMeshes mesh);
+	Mesh* getEnemyMesh();
+
 	static Enemy_Dark* pointer()
 	{
 		return m_pointer;
@@ -25,12 +39,16 @@ private:
 	~Enemy_Dark();
 
 	static Enemy_Dark* m_pointer;
+
 	int movementSpeed;
 	double throwSpeed;
-	Vector3 EnemyDarkPos, DarkBall, EnemyDarkPosOffSet, EnemyDarkScale;
-	Mesh* darkMesh;
-	Vector3 EnemyDarkShadow, DarkBallShadow;
 	double directionToPlayer;
+
+	Vector3 EnemyDarkPos, DarkBall, EnemyDarkPosOffSet, EnemyDarkScale;
+	Vector3 EnemyDarkShadow, DarkBallShadow;
+
+	EnemyDarkMeshes enemyDarkMesh;
+	Mesh *enemyDarkMeshLeft, *enemyDarkMeshRight, *enemyDarkMeshForward, *enemyDarkMeshDownward;
 };
 
 #endif
