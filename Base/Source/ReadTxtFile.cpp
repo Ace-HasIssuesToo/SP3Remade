9fc2990a85d6d46bb0e3657eb02dd5f9f55e5305
TanGlennda <t.glennda@gmail.com>
2016-08-23 13:37:23 +0800

    Edited PokeFont .tga Edited ReadTxtFile .h and .cpp Included call outs of ReadTxtFile in GameState.cpp

--- Base/Source/ReadTxtFile.cpp
+++ Base/Source/ReadTxtFile.cpp
@@ -1,44 +1,70 @@
-//#include "ReadTxtFile.h"
-//#include "Texture_PI.h"
-//#include "Player.h"
-//#include "GameState.h"
-//
-//ReadTxtFile* ReadTxtFile::c_ReadTxtFile = new ReadTxtFile();
-//
-//void ReadTxtFile::Init()
-//{
-//
-//}
-//
-//void ReadTxtFile::ReadFromTextFile(const string name)
-//{
-//	//ifstream inStory;
-//	//string sentence;
-//
-//	//inStory.open("Data//Text//story.txt");
-//
-//	//while (inStory.is_open())
-//	//{
-//	//	getline(inStory, sentence);
-//	//	fullStory.push_back(sentence);
-//	//	cout << "Sentence taken in";
-//	//}
-//	//inStory.close();
-//}
-//
-//void ReadTxtFile::Render()
-//{
-//	//Render_PI::pointer()->modelStack_Set(true);
-//	//Render_PI::pointer()->modelStack_Define(Vector3(Render_PI::Window_Scale().x * 0.5, Render_PI::Window_Scale().y * 0.5, 1), 0, 0, Vector3(150, 100, 1));
-//	////Render_PI::pointer()->RenderMeshIn2D(, false, Vector3(0, 0, 0), Vector3(1, 1, 1));
-//	//Render_PI::pointer()->modelStack_Set(false);
-//}
-//
-//void ReadTxtFile::Exit()
-//{ 
-//	if (c_ReadTxtFile != nullptr)
-//	{
-//		delete c_ReadTxtFile;
-//		c_ReadTxtFile = nullptr;
-//	}
-//}
+#include "ReadTxtFile.h"
+#include "Texture_PI.h"
+#include "Player.h"
+#include "GameState.h"
+
+ReadTxtFile* ReadTxtFile::c_ReadTxtFile = new ReadTxtFile();
+
+ReadTxtFile::ReadTxtFile() : fullIntro(0)
+{
+
+}
+
+ReadTxtFile::~ReadTxtFile()
+{
+
+}
+
+void ReadTxtFile::Init()
+{
+	intro_dialogue = MeshBuilder::GenerateText("intro_dialogue", 16, 16);
+	intro_dialogue->textureArray[0] = LoadTGA("Data//Texture//calibri.tga");
+}
+
+void ReadTxtFile::ReadFromTextFile()
+{
+	//ofstream myfile;
+	//myfile.open("example.txt");
+	//myfile << "Writing this to a file.\n";
+	//myfile.close();
+
+	ifstream inStory;
+	string sentence;
+
+	inStory.open("Data//Text//story.txt");
+	
+	while (!inStory.eof())
+	{
+		getline(inStory, sentence);
+		fullIntro.push_back(sentence);
+		cout << "Sentence taken in" << endl;
+	}
+	inStory.close();
+}
+
+void ReadTxtFile::Render()
+{
+	Render_PI::pointer()->modelStack_Set(true);
+	std::ostringstream ss;
+	ss.precision(0);
+	for (int i = 0; i < fullIntro.size(); i++)
+	{
+		ss << fullIntro[i] << endl;
+	}
+	Render_PI::pointer()->RenderTextOnScreen(intro_dialogue, ss.str(), Color(1, 1, 1), (Render_PI::Window_Scale(), 1, 1), Vector3(5, 5, 1));
+	Render_PI::pointer()->modelStack_Set(false);
+}
+
+void ReadTxtFile::Exit()
+{ 
+	if (intro_dialogue != nullptr)
+	{
+		delete intro_dialogue;
+		intro_dialogue = nullptr;
+	}
+	if (c_ReadTxtFile != nullptr)
+	{
+		delete c_ReadTxtFile;
+		c_ReadTxtFile = nullptr;
+	}
+}
